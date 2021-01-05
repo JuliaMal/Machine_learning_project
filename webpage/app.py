09faadc971ec2ed9ca3b3a098948a26d2ea2e32e
Malickaja <julia.malickaja@sap.com>
2021-01-05 00:29:21 +0000

    Testing input/output

--- webpage/app.py
+++ webpage/app.py
@@ -1,44 +1,33 @@
 # flask for web app.
 #import flask as fl
-from flask import Flask, request
+from flask import Flask, render_template, Response, request, redirect, url_for, jsonify
 # numpy for numerical work.
 import numpy as np
 
 # Create a new web app.
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 # Add root route.
 @app.route("/")
-def index():
+def home():
   return render_template('index.html')
 
-# Add uniform route.
-@app.route('/api/uniform')
-def uniform():
-  return {"value": np.random.uniform()}
+@app.route('/numbers')
+def add_numbers():
+    #result = 0
+    a = request.args.get('a', 0, type=int)
+    # b = request.args.get('b', 0, type=int)
+    #y = a + 2
+    #result = a + a
+    #return jsonify(result = a * a)
+    print(a*a)
+    y = a+7
+    return jsonify(result=y)
 
-# Add normal route.
-@app.route('/api/normal')
-def normal():
-  return {"value": np.random.normal()}
-
-# Add other route.
-@app.route('/api/prediction')
-def prediction():
-  x = 3
-  y = x + 2
-  return {"value": y}
-
-# Reading input
-@app.route('/api/speed')
-def windspeed():
-    x = 5
-    return {"value": x}
-
-@app.route('/get-speed', methods=['GET', 'POST'])
-def foo():
-    bar = request.form['wind']
-    return {value: bar}
+#background process happening without any refreshing
+#@app.route('/api/normal')
+#def normal():
+  #return {"value": np.random.normal()}
 
 if __name__ == '__main__':
     app.run()
